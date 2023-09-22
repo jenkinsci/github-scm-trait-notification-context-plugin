94acec6ca967f4ec015a30b70866d532ec83efca
Ross Bender <l3ender@users.noreply.github.com>
2023-09-22 09:13:01 -0500

    add support for multiple github statuses

--- src/main/java/org/jenkinsci/plugins/githubScmTraitNotificationContext/NotificationContextTrait.java
+++ src/main/java/org/jenkinsci/plugins/githubScmTraitNotificationContext/NotificationContextTrait.java
@@ -1,5 +1,20 @@
 package org.jenkinsci.plugins.githubScmTraitNotificationContext;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jenkinsci.plugins.github_branch_source.AbstractGitHubNotificationStrategy;
+import org.jenkinsci.plugins.github_branch_source.GitHubNotificationContext;
+import org.jenkinsci.plugins.github_branch_source.GitHubNotificationRequest;
+import org.jenkinsci.plugins.github_branch_source.GitHubSCMBuilder;
+import org.jenkinsci.plugins.github_branch_source.GitHubSCMSource;
+import org.jenkinsci.plugins.github_branch_source.GitHubSCMSourceContext;
+import org.jenkinsci.plugins.github_branch_source.PullRequestSCMHead;
+import org.kohsuke.stapler.DataBoundConstructor;
+
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.model.TaskListener;
@@ -10,22 +25,20 @@ import jenkins.scm.api.trait.SCMBuilder;
 import jenkins.scm.api.trait.SCMSourceContext;
 import jenkins.scm.api.trait.SCMSourceTrait;
 import jenkins.scm.api.trait.SCMSourceTraitDescriptor;
-import org.jenkinsci.plugins.github_branch_source.*;
-import org.kohsuke.stapler.DataBoundConstructor;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
 
 public class NotificationContextTrait extends SCMSourceTrait {
 
     private String contextLabel;
     private boolean typeSuffix;
+    private boolean multipleStatuses;
+    private String multipleStatusDelimiter;
 
     @DataBoundConstructor
-    public NotificationContextTrait(String contextLabel, boolean typeSuffix) {
+    public NotificationContextTrait(String contextLabel, boolean typeSuffix, boolean multipleStatuses, String multipleStatusDelimiter) {
         this.contextLabel = contextLabel;
         this.typeSuffix = typeSuffix;
+        this.multipleStatuses = multipleStatuses;
+        this.multipleStatusDelimiter = multipleStatusDelimiter;
     }
 
     public String getContextLabel() {
@@ -36,11 +49,19 @@ public class NotificationContextTrait extends SCMSourceTrait {
         return typeSuffix;
     }
 
+    public boolean isMultipleStatuses() {
+        return multipleStatuses;
+    }
+
+    public String getMultipleStatusDelimiter() {
+        return multipleStatusDelimiter;
+    }
+
     @Override
     protected void decorateContext(SCMSourceContext<?, ?> context) {
         GitHubSCMSourceContext githubContext = (GitHubSCMSourceContext) context;
         githubContext.withNotificationStrategies(Collections.singletonList(
-                new CustomContextNotificationStrategy(contextLabel, typeSuffix)));
+                new CustomContextNotificationStrategy(contextLabel, typeSuffix, multipleStatuses, multipleStatusDelimiter)));
     }
 
     @Override
@@ -77,35 +98,55 @@ public class NotificationContextTrait extends SCMSourceTrait {
 
         private String contextLabel;
         private boolean typeSuffix;
+        private boolean multipleStatuses;
+        private String multipleStatusDelimiter;
 
-        CustomContextNotificationStrategy(String contextLabel, boolean typeSuffix) {
+        CustomContextNotificationStrategy(String contextLabel, boolean typeSuffix, boolean multipleStatuses, String multipleStatusDelimiter) {
             this.contextLabel = contextLabel;
             this.typeSuffix = typeSuffix;
+            this.multipleStatuses = multipleStatuses;
+            this.multipleStatusDelimiter = multipleStatusDelimiter;
         }
 
-        private String buildContext(GitHubNotificationContext notificationContext) {
+        private String buildContext(GitHubNotificationContext notificationContext, String label) {
             SCMHead head = notificationContext.getHead();
             if (typeSuffix) {
                 if (head instanceof PullRequestSCMHead) {
                     if (((PullRequestSCMHead) head).isMerge()) {
-                        return contextLabel + "/pr-merge";
+                        return label + "/pr-merge";
                     } else {
-                        return contextLabel + "/pr-head";
+                        return label + "/pr-head";
                     }
                 } else {
-                    return contextLabel + "/branch";
+                    return label + "/branch";
                 }
             }
-            return contextLabel;
+            return label;
+        }
+
+        private GitHubNotificationRequest buildNotification(GitHubNotificationContext notificationContext, TaskListener listener, String label) {
+            return GitHubNotificationRequest.build(buildContext(notificationContext, label),
+                    notificationContext.getDefaultUrl(listener),
+                    notificationContext.getDefaultMessage(listener),
+                    notificationContext.getDefaultState(listener),
+                    notificationContext.getDefaultIgnoreError(listener));
         }
 
         @Override
         public List<GitHubNotificationRequest> notifications(GitHubNotificationContext notificationContext, TaskListener listener) {
-            return Collections.singletonList(GitHubNotificationRequest.build(buildContext(notificationContext),
-                    notificationContext.getDefaultUrl(listener),
-                    notificationContext.getDefaultMessage(listener),
-                    notificationContext.getDefaultState(listener),
-                    notificationContext.getDefaultIgnoreError(listener)));
+            List<GitHubNotificationRequest> notifications = new ArrayList<>();
+            if (this.multipleStatuses && StringUtils.isNotBlank(multipleStatusDelimiter)) {
+                String[] contextLabels = StringUtils.split(contextLabel, multipleStatusDelimiter);
+                for (String label : contextLabels) {
+                    label = StringUtils.trim(label);
+                    if (StringUtils.isNotBlank(label)) {
+                        notifications.add(buildNotification(notificationContext, listener, label));
+                    }
+                }
+            } else {
+                notifications.add(buildNotification(notificationContext, listener, contextLabel));
+            }
+            return Collections.unmodifiableList(notifications);
         }
 
         @Override
@@ -113,13 +154,13 @@ public class NotificationContextTrait extends SCMSourceTrait {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             CustomContextNotificationStrategy that = (CustomContextNotificationStrategy) o;
-            return typeSuffix == that.typeSuffix &&
-                    Objects.equals(contextLabel, that.contextLabel);
+            return Objects.equals(contextLabel, that.contextLabel) && typeSuffix == that.typeSuffix
+                    && multipleStatuses == that.multipleStatuses && Objects.equals(multipleStatusDelimiter, that.multipleStatusDelimiter);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(contextLabel, typeSuffix);
+            return Objects.hash(contextLabel, typeSuffix, multipleStatuses, multipleStatusDelimiter);
         }
     }
 }
